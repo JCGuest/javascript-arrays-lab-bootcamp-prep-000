fcacaa9b761c32378faf79f7f70975104e119efa
John Guest <jcguest90@gmail.com>
2019-07-10 18:44:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var kittens = [ 'Milo', 'Otis', 'Garfield' ] //define your array here
 
 function destuctivelyAppendKitten(luke){
-  return kittes
+  return kittes.concat
 }
 
 // Add your functions and code here
